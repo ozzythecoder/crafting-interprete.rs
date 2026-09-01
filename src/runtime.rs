1c4f0f1340d16058e6c55c6c649bf917bc2e196e
ozzythecoder <august.chm@gmail.com>
2026-09-01 16:33:53 -0500

    moved runtime to its own file

---
+++ src/runtime.rs
@@ -0,0 +1,123 @@
+use std::{env::Args, fs, io::{BufRead, Write}, path::PathBuf, process};
+
+use crate::{expression::{RuntimeError, interpret}, parser::Parser, scanner::Scanner};
+
+
+pub struct Lox {
+    had_error: bool,
+    had_runtime_error: bool,
+}
+
+impl Lox {
+    pub fn new() -> Self {
+        Lox {
+            had_error: false,
+            had_runtime_error: false,
+        }
+    }
+
+    pub fn main(&mut self, args: &mut Args) {
+        if args.len() > 2 {
+            println!("Usage: jlox [script]");
+            process::exit(64);
+        } else {
+            args.next(); // discard first arg, which is the project filename
+            match args.next() {
+                Some(path) => self.run_file(path),
+                None => self.run_prompt(),
+            };
+        }
+    }
+
+    fn run_file(&mut self, path: String) {
+        match fs::read(PathBuf::from(&path)) {
+            Ok(bytes) => {
+                self.run(bytes);
+                if self.had_error {
+                    process::exit(65);
+                }
+                if self.had_runtime_error {
+                    process::exit(70);
+                }
+            }
+            Err(e) => {
+                println!("Error occurred when reading file at {}", &path);
+                println!("{:?}", e);
+                process::exit(65);
+            }
+        }
+    }
+
+    fn run_prompt(&mut self) {
+        clear_screen();
+        println!("Welcome to the Lox REPL!");
+        println!("Press Ctrl+D to exit.\n");
+
+        let stdin = std::io::stdin();
+        loop {
+            print!("> ");
+            std::io::stdout().flush().unwrap();
+
+            let mut line = String::new();
+            match stdin.lock().read_line(&mut line) {
+                Ok(0) => {
+                    process::exit(0);
+                }
+                Ok(_) => {
+                    self.run(line.into_bytes());
+                    self.had_error = false;
+                }
+                Err(_) => self.error(format!("Error: could not read stdin"), None),
+            };
+        }
+    }
+
+    fn error(&mut self, msg: String, line: Option<usize>) {
+        if let Some(l) = line {
+            println!("[{:?}] Error: {}", l, msg);
+        } else {
+            println!("Error: {}", msg);
+        }
+        self.had_error = true;
+    }
+
+    fn runtime_error(&mut self, error: RuntimeError) {
+        if let Some(line) = error.token.line {
+            println!("[{line}] Runtime Error: {}", error.message);
+        } else {
+            println!("Runtime Error: {}", error.message);
+        }
+        self.had_runtime_error = true;
+    }
+
+    fn run(&mut self, bytes: Vec<u8>) {
+        let source = match String::from_utf8(bytes) {
+            Ok(src) => src,
+            Err(e) => {
+                self.had_error = true;
+                println!("Invalid UTF-8: {e}");
+                return;
+            }
+        };
+        let tokens = Scanner::new(source).scan_tokens();
+        let ast = match Parser::new(tokens).parse() {
+            Ok(tree) => tree,
+            Err(e) => {
+                self.error(e.message, e.token.line);
+                return;
+            }
+        };
+        match interpret(ast) {
+            Ok(result) => {
+                println!("{:?}", result);
+            }
+            Err(e) => {
+                self.runtime_error(e);
+            }
+        };
+    }
+}
+
+fn clear_screen() {
+    print!("\x1B[2J");
+}
