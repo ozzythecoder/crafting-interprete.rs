fb1f3ad27901e6aecade2b01e48898bf3c6801fd
ozzythecoder <august.chm@gmail.com>
2026-09-01 11:42:01 -0500

    documentation; add boolean and nil literals

--- src/expression.rs
+++ src/expression.rs
@@ -1,5 +1,6 @@
 use crate::token::Token;
 
+/// Defines all expression types, as defined in [the grammar definition](./lox_grammar.txt).
 #[derive(Debug, Clone)]
 pub enum Expr {
     Binary(Binary),
@@ -8,6 +9,8 @@ pub enum Expr {
     Literal(Literal),
 }
 
+/// An expression that compares or operates on two expressions.
+/// Examples: `(1 * 2)`, `(3 < 4)`
 #[derive(Debug, Clone)]
 pub struct Binary {
     pub left: Box<Expr>,
@@ -15,22 +18,29 @@ pub struct Binary {
     pub right: Box<Expr>,
 }
 
+/// An expression that operates on a single expression.
+/// Example: `(- 1)`
 #[derive(Debug, Clone)]
 pub struct Unary {
     pub operator: Token,
     pub right: Box<Expr>,
 }
 
+/// An arbitrary expression grouping.
 #[derive(Debug, Clone)]
 pub struct Grouping {
     pub expression: Box<Expr>,
 }
 
-#[derive(Debug, Clone)]
+/// A literal value. Can be a string, 32-bit integer, or 32-bit float.
+#[derive(Debug, Clone, PartialEq)]
 pub enum Literal {
     String(String),
     Int(i32),
     Float(f32),
+    True,
+    False,
+    Nil,
 }
 
 impl ToString for Literal {
@@ -39,10 +49,15 @@ impl ToString for Literal {
             Literal::String(s) => s.clone(),
             Literal::Int(i) => i.to_string(),
             Literal::Float(f) => f.to_string(),
+            Literal::True => "true".to_owned(),
+            Literal::False => "false".to_owned(),
+            Literal::Nil => "nil".to_owned(),
         }
     }
 }
 
+// The book implements a visitor pattern and instructs on writing a codegen tool to declare
+// each node on the AST. Rust's enum types allow us to bypass this step completely.
 pub fn print(expr: &Expr) -> String {
     match expr {
         Expr::Binary(b) => parenthesize(&b.operator.lexeme, &[b.left.clone(), b.right.clone()]),
