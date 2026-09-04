18fe9ec7c156e7f83efdacde6ed5e44a55068d21
ozzythecoder <august.chm@gmail.com>
2026-09-04 09:50:48 -0500

    var statement definition

--- src/statement.rs
+++ src/statement.rs
@@ -1,7 +1,8 @@
-use crate::expression::Expr;
+use crate::{expression::Expr, token::Token};
 
 #[derive(Debug, Clone)]
 pub enum Stmt {
     Expression(Expr),
     Print(Expr),
+    Var { name: Token, initializer: Option<Expr> },
 }
