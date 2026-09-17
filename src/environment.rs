9eaee7a9715450e0b946bc08b14aea9925f1e053
ozzythecoder <august.chm@gmail.com>
2026-09-17 10:23:11 -0500

    Environment refactor
    Environment values are now Values instead of Literals. The enclosing
    environment is an Rc<RefCell<>, rather than a raw environment. Rest of
    the methods updated to reflect this.

--- src/environment.rs
+++ src/environment.rs
@@ -1,60 +1,60 @@
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-use crate::{expression::Literal, interpreter::RuntimeError, token::Token};
+use crate::{expression::Value, interpreter::{Interrupt, RuntimeError}, token::Token};
 
 #[derive(Default)]
 pub struct Environment {
-    pub values: HashMap<String, Literal>,
-    pub enclosing: Option<Box<Environment>>,
+    pub values: HashMap<String, Value>,
+    pub enclosing: Option<Rc<RefCell<Environment>>>,
 }
 
 impl Environment {
-    pub fn new(enclosing: Option<Environment>) -> Self {
+    pub fn new(enclosing: Option<Rc<RefCell<Environment>>>) -> Self {
         Environment {
             values: HashMap::new(),
-            enclosing: if let Some(e) = enclosing {
-                Some(Box::new(e))
-            } else {
-                None
-            },
+            enclosing,
         }
     }
 
-    pub fn define(&mut self, token: &Token, value: Literal) {
+    pub fn define(&mut self, token: &Token, value: Value) {
         self.values.insert(token.lexeme.to_owned(), value);
     }
 
-    pub fn get(&self, token: &Token) -> Result<&Literal, RuntimeError> {
+    pub fn define_native(&mut self, name: &str, value: Value) {
+        self.values.insert(name.to_owned(), value);
+    }
+
+    pub fn get(&self, token: &Token) -> Result<Value, Interrupt<RuntimeError>> {
         if let Some(v) = self.get_var(token) {
-            Ok(v)
+            Ok(v.clone())
         } else if let Some(env) = &self.enclosing {
-            env.get(token)
+            env.borrow().get(token)
         } else {
-            Err(self.undefined_var(token))
+            Err(self.undefined_var(token).wrap())
         }
     }
 
-    fn get_var(&self, token: &Token) -> Option<&Literal> {
+    fn get_var(&self, token: &Token) -> Option<&Value> {
         self.values.get(&token.lexeme)
     }
 
-    pub fn assign(&mut self, token: &Token, value: &Literal) -> Result<(), RuntimeError> {
+    pub fn assign(&mut self, token: &Token, value: &Value) -> Result<(), Interrupt<RuntimeError>> {
         if let Some(_) = self.get_var(token) {
             self.assign_var(token, value)
         } else if self.enclosing.is_some() {
             let env = self.enclosing.as_mut().expect("Quantum nonsense");
-            env.assign(token, value)
+            env.borrow_mut().assign(token, value)
         } else {
-            Err(self.undefined_var(token))
+            Err(self.undefined_var(token).wrap())
         }
     }
 
-    fn assign_var(&mut self, token: &Token, value: &Literal) -> Result<(), RuntimeError> {
+    fn assign_var(&mut self, token: &Token, value: &Value) -> Result<(), Interrupt<RuntimeError>> {
         if self.values.contains_key(&token.lexeme) {
             self.values.insert(token.lexeme.to_owned(), value.clone());
             Ok(())
         } else {
-            Err(self.undefined_var(token))
+            Err(self.undefined_var(token).wrap())
         }
     }
 
